4624372241b96fde1dcca7ac33ab6806f6d2c152
Samuel Schlesinger <sgschlesinger@gmail.com>
2026-08-27 07:16:05 -0400

    Add canonical quotient interaction bounds

--- Algebraic/LowerBound/Fusion.lean
+++ Algebraic/LowerBound/Fusion.lean
@@ -7,6 +7,7 @@ import Algebraic.LowerBound.Fusion.Arithmetic.Interaction
 import Algebraic.LowerBound.Fusion.Arithmetic.Interaction.Rank
 import Algebraic.LowerBound.Fusion.Arithmetic.Interaction.Multiple
 import Algebraic.LowerBound.Fusion.Arithmetic.Interaction.Linear
+import Algebraic.LowerBound.Fusion.Arithmetic.Interaction.Linear.Quotient
 import Algebraic.LowerBound.Fusion.Arithmetic.Interaction.Polynomial
 import Algebraic.LowerBound.Fusion.Arithmetic.Interaction.Polynomial.Mixing
 import Algebraic.LowerBound.Fusion.Arithmetic.Interaction.Polynomial.Squarefree
